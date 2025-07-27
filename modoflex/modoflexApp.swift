83ea95373a43013561fdd7f4e09f079a8c68aa57
Sandor Orszagh Bihari <sandor.orszagh@profession.hu>
2025-07-27 18:47:51 +0200

    Initial Commit

---
+++ modoflex/modoflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  modoflexApp.swift
+//  modoflex
+//
+//  Created by Orszagh Bihari Sandor  on 2025. 07. 27..
+//
+
+import SwiftUI
+
+@main
+struct modoflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
